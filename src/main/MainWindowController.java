d39eda4512471d860d89a2a5141728edb66b956b
Andrei Shakarov <andrei.shakarov@gmail.com>
2017-10-11 00:58:35 +0300

    adding working slider

--- src/main/MainWindowController.java
+++ src/main/MainWindowController.java
@@ -1,11 +1,17 @@
 package main;
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainWindowController {
 
@@ -27,22 +33,41 @@ public class MainWindowController {
     public MainWindowController() {
         refreshThread.start();
         refreshThread.addListeners(() -> updateLabels());
+        Platform.runLater(() -> {
+            double currentLockTime = getCurrentLockTime();
+            slider.setValue(currentLockTime);
+            timeLock.setText(Integer.toString((int)currentLockTime) + " seconds");
+            slider.valueProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                    timeLock.setText(Integer.toString((int)slider.getValue()) + " seconds");
+                    apply.setDisable(false);
+                }
+            });
+        });
     }
 
     private void updateLabels() {
         Platform.runLater(() -> {
-                if(battery != null) battery.setText(refreshThread.getList().get(0) + ", " + refreshThread.getList().get(1));
+                if(battery != null) battery.setText(refreshThread.getList().get(0) + ", "
+                        + refreshThread.getList().get(1));
                 if(time != null) time.setText(refreshThread.getList().get(2));
                 if(adapter != null) adapter.setText(refreshThread.getAdapterInfo());
         });
     }
 
-    public void handle(){
-        System.out.println("handle");
+    private double getCurrentLockTime() {
+        try {
+            Process process = Runtime.getRuntime().exec("gsettings get org.gnome.desktop.session idle-delay");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            return Integer.parseInt(reader.readLine().split(" ")[1]);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
     public void shutdown() {
-        System.out.println("Shutdown");
         refreshThread.shutdown();
     }
 
